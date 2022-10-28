e117f0ad159882ed316260d650d9afd4ee5d4989
XDD44 <50290408+xdd44-utopia@users.noreply.github.com>
2022-10-27 23:44:56 -0400

    Ortho generation succeeded

--- Model 3.py
+++ Model 3.py
@@ -5,12 +5,53 @@ import rhinoscriptsyntax as rs
 import scriptcontext as sc
 from math import *
 
+cuttingPos = [
+    [
+        [
+            [1, 1, 1],
+            [1, -1, 1],
+            [1, -1, -1],
+            [1, 1, -1]
+        ],
+        [
+            [1, 1, 1],
+            [-1, 1, 1],
+            [-1, 1, -1],
+            [1, 1, -1]
+        ],
+        [
+            [1, 1, 1],
+            [-1, 1, 1],
+            [-1, -1, 1],
+            [1, -1, 1]
+        ]
+    ],
+    [
+        [
+            [-1, 1, 1],
+            [-1, -1, 1],
+            [-1, -1, -1],
+            [-1, 1, -1]
+        ],
+        [
+            [1, -1, 1],
+            [-1, -1, 1],
+            [-1, -1, -1],
+            [1, -1, -1]
+        ],
+        [
+            [1, 1, -1],
+            [-1, 1, -1],
+            [-1, -1, -1],
+            [1, -1, -1]
+        ]
+    ]
+]
+
 def x1(u, v):
-    u += v
     return cos(u) * sin(v) + (1 * cos(3 * u) + 3 * cos(5 * u)) / 24
 
 def y1(u, v):
-    u += v
     return sin(u) * sin(v) + (1 * sin(3 * u) + 3 * sin(5 * u)) / 24
 
 def z1(u, v):
@@ -27,30 +68,74 @@ def y2(u, v):
     return sin(u) * sin(v) + sin(3 * u) / 4
 
 def z2(u, v):
-    return cos(v) * 1.125
+    return cos(v)
 
-def generate(x, y, z, ustep, ufreq, vstep):
+def generate(x, y, z, ustep, ufreq, vstep, cuttingInterval, thickness, coneTopDist, direction):
 
-    piecesid = [[] for i in range(int(pi * 2 / ustep))]
-    piecesm = [[] for i in range(int(pi * 2 / ustep))]
-    intersectsid = [[None] * int(pi / vstep / 2) for i in range(int(pi * 2 / ufreq))]
-    cuttingPlaneids = []
+    # direction 0: yz, 1: xz, 2: xy
 
-    for u in frange(0, pi * 2, ufreq):
-        planeS = Point3d(x[0](u, vfreq * 2) * offsetx[0], y[0](u, vfreq * 2) * offsety[0], z[0](u, vfreq * 2))
-        planeT = Point3d(x[0](u, vfreq * 6) * offsetx[0], y[0](u, vfreq * 6) * offsety[0], z[0](u, vfreq * 6))
-        middleV = (planeS + planeT) / 2
-        middleV[2] = 0
-        middleV = normalize(middleV)
-        middleV = Point3d(middleV[0], middleV[1], middleV[2]) / 10
+    coneTop = Point3d(
+        coneTopDist * offsetx if direction == 0 else 0,
+        coneTopDist * offsetx if direction == 1 else 0,
+        coneTopDist * offsetx if direction == 2 else 0
+    )
 
-        surface = NurbsSurface.CreateFromCorners(
-            Point3d(planeS + middleV * 10),
-            Point3d(planeS),
-            Point3d(planeT),
-            Point3d(planeT + middleV * 10)
+    cuttingPlanes = []
+    pieces = [[] for i in range(int(pi * 2 / ustep))]
+
+    for v in frange(cuttingInterval, y(pi / 2, pi / 2) * offsetx, cuttingInterval):
+        px = v if direction == 0 else x(0, pi / 2) * offsetx
+        py = v if direction == 1 else y(pi / 2, pi / 2) * offsety
+        pz = v if direction == 2 else z(0, pi) * offsetz
+
+        surface1 = NurbsSurface.CreateFromCorners(
+            Point3d(
+                cuttingPos[0][direction][0][0] * px,
+                cuttingPos[0][direction][0][1] * py,
+                cuttingPos[0][direction][0][2] * pz
+            ),
+            Point3d(
+                cuttingPos[0][direction][1][0] * px,
+                cuttingPos[0][direction][1][1] * py,
+                cuttingPos[0][direction][1][2] * pz
+            ),
+            Point3d(
+                cuttingPos[0][direction][2][0] * px,
+                cuttingPos[0][direction][2][1] * py,
+                cuttingPos[0][direction][2][2] * pz
+            ),
+            Point3d(
+                cuttingPos[0][direction][3][0] * px,
+                cuttingPos[0][direction][3][1] * py,
+                cuttingPos[0][direction][3][2] * pz
+            )
         )
-        cuttingPlaneids.append(sc.doc.Objects.AddSurface(surface))
+        surface2 = NurbsSurface.CreateFromCorners(
+            Point3d(
+                cuttingPos[1][direction][0][0] * px,
+                cuttingPos[1][direction][0][1] * py,
+                cuttingPos[1][direction][0][2] * pz
+            ),
+            Point3d(
+                cuttingPos[1][direction][1][0] * px,
+                cuttingPos[1][direction][1][1] * py,
+                cuttingPos[1][direction][1][2] * pz
+            ),
+            Point3d(
+                cuttingPos[1][direction][2][0] * px,
+                cuttingPos[1][direction][2][1] * py,
+                cuttingPos[1][direction][2][2] * pz
+            ),
+            Point3d(
+                cuttingPos[1][direction][3][0] * px,
+                cuttingPos[1][direction][3][1] * py,
+                cuttingPos[1][direction][3][2] * pz
+            )
+        )
+        cuttingPlanes = [sc.doc.Objects.AddSurface(surface1)] + cuttingPlanes + [sc.doc.Objects.AddSurface(surface2)]
+    
+    intersects = [[] for i in range(len(cuttingPlanes))]
+    results = []
 
     for i, u in enumerate(frange(0, pi * 2, ustep)):
 
@@ -59,61 +144,94 @@ def generate(x, y, z, ustep, ufreq, vstep):
             if (v <= vfreq * 2 - vstep or v > pi - vfreq * 2 - vstep):
                 continue
             
-            for f in range(len(x)):
-                uu = u + ustep
-                vv = v + vstep
-                cur = Point3d(x[f](u, v) * offsetx[f], y[f](u, v) * offsety[f], z[f](u, v))
-                nex = Point3d(x[f](u, vv) * offsetx[f], y[f](u, vv) * offsety[f], z[f](u, vv))
+            uu = u + ustep
+            vv = v + vstep
 
-                uu = u + ustep
-                vv = v + vstep
-                surface = NurbsSurface.CreateFromCorners(
-                    Point3d(x[0](u, v) * offsetx[0], y[0](u, v) * offsety[0], z[0](u, v)),
-                    Point3d(x[0](uu, v) * offsetx[0], y[0](uu, v) * offsety[0], z[0](uu, v)),
-                    Point3d(x[0](uu, vv) * offsetx[0], y[0](uu, vv) * offsety[0], z[0](uu, vv)),
-                    Point3d(x[0](u, vv) * offsetx[0], y[0](u, vv) * offsety[0], z[0](u, vv))
-                )
-                piecesid[i].append(sc.doc.Objects.AddSurface(surface))
-                piecesm[i].append((
-                    Point3d(x[0](uu, v) * offsetx[0], y[0](uu, v) * offsety[0], z[0](uu, v)) + 
-                    Point3d(x[0](u, vv) * offsetx[0], y[0](u, vv) * offsety[0], z[0](u, vv))
-                ) / 2)
+            uu = u + ustep
+            vv = v + vstep
+            surface = NurbsSurface.CreateFromCorners(
+                Point3d(x(u, v) * offsetx, y(u, v) * offsety, z(u, v) * offsetz),
+                Point3d(x(uu, v) * offsetx, y(uu, v) * offsety, z(uu, v) * offsetz),
+                Point3d(x(uu, vv) * offsetx, y(uu, vv) * offsety, z(uu, vv) * offsetz),
+                Point3d(x(u, vv) * offsetx, y(u, vv) * offsety, z(u, vv) * offsetz)
+            )
+            pieces[i].append(sc.doc.Objects.AddSurface(surface))
 
-    for k, cuttingid in enumerate(cuttingPlaneids):
-        for j in range(len(piecesid[i])):
-            for i in range(len(piecesid)):
-                intersectid = rs.IntersectBreps(cuttingid, piecesid[i][j])
-                if (intersectid != None):
-                    intersectsid[k][j] = rs.coercecurve(intersectid[0])
-                    segment = Polyline((intersectsid[k][j].PointAtStart, intersectsid[k][j].PointAtEnd))
-                    sc.doc.Objects.AddPolyline(segment)
-                    segment = Line(intersectsid[k][j].PointAtStart, intersectsid[k][j].PointAtEnd)
-                    closestPoint = segment.ClosestPoint(piecesm[i][j], False)
-                    closestV = normalize(piecesm[i][j] - closestPoint)
-                    closestV = Point3d(closestV[0], closestV[1], closestV[2]) / 10
-                    if (closestV[2] > 0):
-                        closestV = - closestV
-                    otherPoint = closestPoint + closestV
-                    normal = Polyline((closestPoint, otherPoint))
-                    sc.doc.Objects.AddPolyline(normal)
-
-xf = [x1]
-yf = [y1]
-zf = [z1]
-offsetx = [1]
-offsety = [1]
-
-# xf = [x1, x1]
-# yf = [y1, y1]
-# zf = [z1, z1]
-# offsetx = [1, 0.9]
-# offsety = [1, 0.9]
+    def extendPos(base):
+        v = base - (1 if base[direction] > 0 else -1) * coneTop
+        v = normalize(v)
+        v = Point3d(v[0], v[1], v[2]) * thickness
+        return base + v
     
-ustep = pi / 256
-ufreq = ustep * 64
+    def mirrorPos(base, mirrorPlane):
+        # mirrorPlane = 0 : yz, 1 : xz, 2 : xy
+        v = base - (1 if base[direction] > 0 else -1) * coneTop
+        v = normalize(v)
+        v = Point3d(v[0], v[1], v[2]) * thickness
+        v[mirrorPlane] = - v[mirrorPlane]
+        v = - v
+        return base + v
+
+    for k, cutting in enumerate(cuttingPlanes):
+        result = []
+        for j in range(len(pieces[i])):
+            for i in range(len(pieces)):
+                intersect = rs.IntersectBreps(cutting, pieces[i][j])
+                if (intersect != None):
+                    intersects[k].append(rs.coercecurve(intersect[0]))
+                    sBase = intersects[k][-1].PointAtStart
+                    sExtend = extendPos(sBase)
+                    sMirror = mirrorPos(sBase, direction)
+                    tBase = intersects[k][-1].PointAtEnd
+                    tExtend = extendPos(tBase)
+                    tMirror = mirrorPos(tBase, direction)
+                    surface = NurbsSurface.CreateFromCorners(
+                        sBase,
+                        tBase,
+                        tExtend,
+                        sExtend
+                    )
+                    result.append(sc.doc.Objects.AddSurface(surface))
+                    surface = NurbsSurface.CreateFromCorners(
+                        sBase,
+                        tBase,
+                        tMirror,
+                        sMirror
+                    )
+                    result.append(sc.doc.Objects.AddSurface(surface))
+                    rs.DeleteObject(intersect[0])
+        results.append(rs.JoinSurfaces(result, delete_input = True))
+
+    for cutting in cuttingPlanes:
+        rs.DeleteObject(cutting)
+    for piece in pieces:
+        for p in piece:
+            rs.DeleteObject(p)
+
+offsetx = 100
+offsety = 100
+offsetz = 112.5
+
+ustep = pi / 64
+ufreq = ustep * 16
 vstep = pi / 64
 vfreq = vstep * 8
+cuttingInterval = 25
+thickness = 25
 
-generate(xf, yf, zf, ustep, ufreq, vstep)
+generate(x1, y1, z1, ustep, ufreq, vstep, cuttingInterval, thickness, 1.5, 1)
+
+offsetx = 50
+offsety = 50
+offsetz = 112.5
+
+ustep = pi / 64
+ufreq = ustep * 16
+vstep = pi / 64
+vfreq = vstep * 8
+cuttingInterval = 25
+thickness = 25
+
+generate(x1, y1, z1, ustep, ufreq, vstep, cuttingInterval, thickness, 1.5, 0)
 
 sc.doc.Views.Redraw()
